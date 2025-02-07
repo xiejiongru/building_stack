e3f0b28d2ab1024aea5fd545e671c4ba585b29ed
xiejiongru <xiejiongru@outlook.com>
2025-02-07 14:49:24 +0100

    feat: oop

---
+++ javascript:/home/formation/Desktop/building_stack/src/blocks.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three';
+
+export function createCube() {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    return cube;
+}
